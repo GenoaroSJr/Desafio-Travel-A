8165c8df9187d6fcc83f9e91ad02870846a3cb0d
GenoaroSJr <genoarojr@unifei.edu.br>
2020-03-27 21:24:26 -0300

    Prototype of the home page. Connection with mongodb. Form section.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
+const path = require('path');
 
 const admin = require('./routes/admin');
 
@@ -15,10 +16,18 @@ app.use(bodyParser.json())
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars');
 //Mongoose
+mongoose.Promise = global.Promise; //Avoid Error
+mongoose.connect('mongodb://localhost/travelA').then(()=>{
+    console.log("Connected to the mongo")
+}).catch((err) => {
+    console.log("Error connecting to the mongo "+err)
+})
+//Public
+app.use(express.static(path.join(__dirname,"public")))
 
 //Routes
 app.get('/', (req,res)=>{
-    res.send('Main page')
+    res.render('index')
 })
 
 app.get('/success', (req,res)=>{
--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req,res)=>{
-    res.send("ADM panel main page");
+    res.render("admin/index");
 });
 
 module.exports = router
