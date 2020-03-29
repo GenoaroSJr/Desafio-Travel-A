c2a8bf31e465ef4fc7ab91ff03a17d544815cc07
GenoaroSJr <genoarojr@unifei.edu.br>
2020-03-29 08:13:28 -0300

    Final adjustmests.

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose');
+require("../models/Register");
+const Register = mongoose.model("register");
 
 router.get('/', (req,res)=>{
+    Register.find().then((registers) => {
+        res.render("admin/index", {registers: registers})
+    }).catch((err) => {
+        res.redirect("/")
+        console.log("ERRO: "+err)
+    })
     res.render("admin/index");
 });
 
